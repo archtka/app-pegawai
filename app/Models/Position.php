02ef0442e3a21eca487ae23f4baf012d6cb1b564
archtka <nabilatjdd@gmail.com>
2025-10-27 01:56:03 +0700

    Penambahan migrasi semua tabel

---
+++ app/Models/Position.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Position extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['nama_jabatan', 'gaji_pokok'];
+
+    
+    public function employees()
+    {
+        return $this->hasMany(Employee::class, 'jabatan_id');
+    }
+}
